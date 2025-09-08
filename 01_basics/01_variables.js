44dcfe2e88626e484e627de202ed3954abb3a9bb
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-09-08 17:50:41 +0000

    conatiner config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "sakshi@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+//accountID =2not allowed
+
+accountEmail = "hanwat7@google.com"
+accountPassword = "23456"
+accountCity = "Dewas"
+
+/*Prefer not to use of issue in block scope and functional scope */
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js as newer version
+
+//alert("hello")
+//alert(3 + 3) we are using nodejs, not browser
+
+console.log(3 + 3)
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false 
+
+//number => 2 to power 53
+//bigint -- use whwn number limt reched
+//string => ""
+// boolean => true/false
+// null => standalone value
+//undefined => value not assigned
+// symbol => unique
+
+// object
+console.log(typeof "hitesh")
+console.log(typeof undefined)
+console.log(typeof null)
